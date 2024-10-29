6d38fdb3fef9726ee1626b3f01934322d9004996
ghwrf <ghwrf@qq.com>
2024-10-29 10:04:53 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
@@ -18,3 +18,4 @@
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/Thaolga/openwrt-nekobox  package/openwrt-nekobox
